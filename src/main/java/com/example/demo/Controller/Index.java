6c97f68b64e478f95c1e58b6334c49adb945ccde
iterge <it_lph@126.com>
2018-04-12 12:06:31 +0800

    first commit

---
+++ src/main/java/com/example/demo/Controller/Index.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/4/12.
+ */
+@RestController
+public class Index {
+    @RequestMapping("/")
+    public String indexTest(){
+        return "hello12";
+    }
+}
